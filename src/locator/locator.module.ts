147f288590a25326068c0f4601cca63f2ce0660d
ryzzaki <cuong.nugi@gmail.com>
2019-12-17 10:10:18 +0100

    fix: dependencies and imports

--- src/locator/locator.module.ts
+++ src/locator/locator.module.ts
@@ -3,13 +3,10 @@ import { LocatorService } from './locator.service';
 import { LocatorController } from './locator.controller';
 import { LocationRepository } from './repositories/location.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      LocationRepository,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([LocationRepository]), AuthModule],
   providers: [LocatorService],
   controllers: [LocatorController],
   exports: [LocatorService],
